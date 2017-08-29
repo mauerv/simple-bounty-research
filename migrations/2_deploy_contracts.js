1cdcb6fc3cb6543377478371782203586ac3e38a
Mauro Velazquez <maurovelazquez1@gmail.com>
2017-08-29 18:01:09 -0300

    Delete placeholder contracts and add useful ones.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const ResearchNetwork = artifacts.require("./ResearchNetwork.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(ResearchNetwork);
 };
